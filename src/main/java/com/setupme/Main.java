1a6c07554a55a80c5e2ed8ef0ac3ed8924ea63be
KamilaArleta <kamila5095@gmail.com>
2020-08-21 20:16:40 +0200

    Add Apache Commons IO dependency, Add Apache log4j dependency, Add login function.

--- src/main/java/com/setupme/Main.java
+++ src/main/java/com/setupme/Main.java
@@ -3,19 +3,28 @@ package com.setupme;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.setupme.model.ConfigurationModel;
 import com.setupme.model.FileModel;
+import com.setupme.model.ProgramModel;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.kohsuke.args4j.OptionHandlerFilter.ALL;
 
 public class Main {
+
+    private Logger LOG = Logger.getLogger(Main.class);
+
     @Option(name = "-h", usage = "show help")
     private boolean showHelp;
 
@@ -37,7 +46,6 @@ public class Main {
                 throw new CmdLineException(parser, "No argument is given");
             }
         } catch (CmdLineException e) {
-
             System.out.println(e.getMessage());
             System.out.println("java SampleMain [options...] arguments...");
             parser.printUsage(System.out);
@@ -48,16 +56,22 @@ public class Main {
             System.out.println("Example: java com.setupme.Main" + parser.printExample(ALL));
         }
         if (jsonPath != null) {
-
             ObjectMapper mapper = new ObjectMapper();
             try (FileReader reader = new FileReader(ClassLoader.getSystemResource(jsonPath).getFile())) {
                 FileModel configuration = mapper.readValue(reader, FileModel.class);
-                System.out.println(configuration);
-            }catch (Exception ex) {
+                for (ProgramModel programModel : configuration.getConfiguration().getPrograms()) {
+                    LOG.warn("Im begining download program " + programModel.getName() + " " + programModel.getVersion() + " from link " + programModel.getLink());
+                    FileUtils.copyURLToFile(
+                            new URL(programModel.getLink()),
+                            new File(programModel.getName() + programModel.getVersion() + ".exe"),
+                            10000,
+                            10000
+                    );
+                    LOG.warn("Download complete");
+                }
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
-
         }
     }
 }
